36b1d91c45a7da3d56681f802cd61b3f7995981e
IgorKaan <41728698+IgorKaan@users.noreply.github.com>
2018-08-20 12:22:48 +0300

    Update MotorController.cpp

--- MotorController.cpp
+++ MotorController.cpp
@@ -1,7 +1,8 @@
 #include "MotorController.h"     //библиотека для управления двигателями
 #include "Arduino.h"            //стандартная библиотека  Arduino.h
 #include <SPI.h>                //библиотека для работы с шиной SPI
-#include "esp32-hal-ledc.h"     //библиотека ШИМ
+#include "esp32-hal-ledc.h" //библиотека ШИМ
+#include <cmath>    
 
 //Задаем подключеные соответствующие контакты, для правого и левого двигателей, в очередности pinUpRight, pinDownRight, pinSpeedRight, pinUpLeft, pinDownLeft, pinSpeedLeft
 void MotorController::setup(uint8_t pinEnableR, uint8_t pinForwardR, uint8_t pinBackR , uint8_t pinForwardL, uint8_t pinBackL, uint8_t pinEnableL, uint8_t channel_Right, uint8_t channel_Left)
@@ -45,6 +46,7 @@ void  MotorController::setPinsValuesMotorDriver (motor* motorStruct, bool up, bo
     digitalWrite(motorStruct->pin_forward, up);
 	digitalWrite(motorStruct->pin_back, down);
 	ledcWrite(motorStruct->channel, speed);
+	Serial.println(speed);
 }
 // эта функция обеспечит вращение двигателей в двух направлениях на установленной скорости
 void MotorController::rotation(motor* motorStruct, short Speed, short Side)
@@ -55,52 +57,49 @@ void MotorController::rotation(motor* motorStruct, short Speed, short Side)
 		setPinsValuesMotorDriver(motorStruct, HIGH, LOW, Speed); //настройки запуска двигателя в одну сторону
 	}
 	else if (Side<0) {
-		setPinsValuesMotorDriver(motorStruct, LOW, HIGH, -(Speed));//настройки запуска двигателя в обратную  сторону
+		setPinsValuesMotorDriver(motorStruct, LOW, HIGH, Speed);//настройки запуска двигателя в обратную  сторону
 	}
 	else {
 		setPinsValuesMotorDriver(motorStruct, LOW, LOW, 0); //двигатель отключен
 	}
 }
 // контроль вращения двигателем на основе данных с джойстика
-void MotorController::controlByJoystick(short xCoord, short yCoord, float reduceSpeed)
+void MotorController::controlByJoystick(short xCoord, short yCoord, float reduceSpeed, float reduceSpeedSide)
 {
-	if ((xCoord > -400 && xCoord < 400) && yCoord > 400) {
+	if ((xCoord > -500 && xCoord < 500) && yCoord < -500) {
 
-	    rotation(MotorLeft, -yCoord, -yCoord);
-		rotation(MotorRight, yCoord/reduceSpeed, yCoord);
+	    rotation(MotorLeft, abs(yCoord/reduceSpeed), -yCoord);
+		rotation(MotorRight,abs(yCoord/reduceSpeed), yCoord);
 		Serial.println("Moving forward");
-
 	}
 	
-	else if (xCoord > 400 && yCoord >=0) {
-		rotation(MotorLeft, 0, 0);
-		rotation(MotorRight, yCoord, yCoord);
-		Serial.println("Moving forward left");
-		Serial.println(yCoord/reduceSpeed);
-		Serial.println(yCoord);
+	else if (xCoord >= 500 && yCoord <= 500) {
+		rotation(MotorRight, abs(xCoord/reduceSpeedSide), yCoord);
+		rotation(MotorLeft, abs(xCoord/reduceSpeed/7), -yCoord);
+		Serial.println("Moving right");
 	}
 
-	else if (xCoord < -400 && yCoord >=0) {
-	    rotation(MotorLeft, -yCoord, -yCoord);
-		rotation(MotorRight, 0, 0);
-		Serial.println("Moving forward right");
+	else if (xCoord < -500 && yCoord <= 500) {
+		rotation(MotorRight,abs(xCoord/reduceSpeed/7), yCoord);
+		rotation(MotorLeft, abs(xCoord/reduceSpeedSide), -yCoord);
+		Serial.println("Moving left");
 	}
 
-	else if ((xCoord > -400 && xCoord < 400) && yCoord < -400) {
-	    rotation(MotorLeft, -yCoord, -yCoord);
-		rotation(MotorRight, yCoord, yCoord);
+	else if ((xCoord > -1000 && xCoord < 1000) && yCoord > 50) {
+	    rotation(MotorLeft,  abs(yCoord/reduceSpeed), -yCoord);
+		rotation(MotorRight, abs(yCoord/reduceSpeed), yCoord);
 		Serial.println("Moving back");
 	}
-	else if (xCoord > 400 && yCoord <= 0) {
-		rotation(MotorLeft, 0, 0);
-		rotation(MotorRight, yCoord, yCoord);
-		Serial.println("Moving back left");
-	}
-	else if (xCoord < -400 && yCoord <= 0) {
-	    rotation(MotorLeft, -yCoord, -yCoord);
-		rotation(MotorRight, 0, 0);
-		Serial.println("Moving back right");
-	}
+	//else if (xCoord > 500 && yCoord >= -500) {
+		//rotation(MotorLeft,0/*-yCoord/ (1+(log(10+abs(xCoord)))/4)*/, -yCoord);
+		//rotation(MotorRight, yCoord/reduceSpeed, yCoord);
+		//Serial.println("Moving back left");
+	//}
+	//else if (xCoord < -150 && yCoord <= 0) {
+	   // rotation(MotorLeft, -yCoord/reduceSpeed, -yCoord);
+		//rotation(MotorRight,0/*-yCoord/ (1+(log(10+abs(xCoord)))/4)*/, yCoord);
+		//Serial.println("Moving back right");
+	//}
 	else {
 		rotation(MotorLeft, 0, 0);
 		rotation(MotorRight, 0, 0);
